3fdb0865754ebe1d204b3198db43683675b50ae5
Matthew T. Warkentin <warkentin@lunenfeld.ca>
2022-10-27 12:29:46 -0600

    Initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='project',
+    version='0.0.0.9000',
+    description='Describe Your Project',
+    author='',
+    author_email='',
+    url='https://github.com/',
+    install_requires=['pytorch-lightning'],
+    packages=find_packages(),
+)
